bd2d0921bb202fd5ecb067897cb66d67e1552a54
nathanlau3 <nathanlau3@gmail.com>
2022-06-01 16:26:07 +0700

    Bug fixes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const swaggerUI = require('swagger-ui-express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const config = require('./config/config.js');
 // const specs = require('./swagger.json')
 // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 
@@ -12,6 +13,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended:true,limit:"10mb",type:'
 app.use(urlencodedParser);
 app.use(jsonParser);
 
+console.log(`NODE_ENV=${config.PORT}`);
 //routers
 const ProductRoute = require('./routes/ProductRoute');
 app.use('/Product', ProductRoute);
@@ -28,4 +30,4 @@ app.use('/Post', PostRoute);
 const FollowRoute = require('./routes/FollowRoute');
 app.use('/Follow', FollowRoute);
 
-app.listen(process.env.PORT, () => console.log("port berjalan di " + `${process.env.PORT}`));
+app.listen(config.PORT, () => console.log("port berjalan di " + `${config.PORT}`));
